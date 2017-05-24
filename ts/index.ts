80396df9a62c2d1ab5dba23510075dff8b545be8
Mohamed Al-Rifay <mohamed.abdelattif@fci.helwan.edu.eg>
2017-05-25 01:17:26 +0200

    Set canvas alpha to false
    Add 2d bricks
    add jsdoc
    add brick class

--- ts/index.ts
+++ ts/index.ts
@@ -1,6 +1,6 @@
 const CanvasGame: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById('canvas-game');
 const Body: HTMLBodyElement = <HTMLBodyElement>document.body;
-const CanvasContext: CanvasRenderingContext2D = CanvasGame.getContext('2d');
+const CanvasContext: CanvasRenderingContext2D = CanvasGame.getContext('2d', { alpha: false });
 const FramesBerSecond: number = 60;
 //Mouse position
 let MouseX = 0;
@@ -19,10 +19,11 @@ const PaddleGap: number = 20;
 let paddleX: number = 0;
 let paddleY: number = 0;
 //Brick
-const BrickWidth: number = 100;
-const BrickHeight: number = 50;
-const BricksCount: number = 11;
-const BrickGrid: boolean[] = new Array(BricksCount);
+const BrickWidth: number = 80;
+const BrickHeight: number = 30;
+const BrickLinesCount: number = 8;
+const BricksCountBerLine: number = 14;
+const BrickGrid: boolean[][] = new Array(BrickLinesCount);
 
 function hex6(num: number, s: number): string {
     let n = num.toString(16);
@@ -63,10 +64,15 @@ function resetBall(): void {
     ballY = CanvasGame.height / 2;
     ballXPos = ballYPos = 4;
 }
-
+let BrickGridColor = new Array(BrickLinesCount);
 function resetBricks(): void {
-    for (let i = 0; i < BricksCount; i++) {
-        BrickGrid[i] = Math.random() > 0.5;
+    for (let i = 0; i < BrickLinesCount; i++) {
+        BrickGrid[i] = new Array(BricksCountBerLine);
+        BrickGridColor[i] = new Array(BricksCountBerLine);
+        for (var j = 0; j < BricksCountBerLine; j++) {
+            BrickGrid[i][j] = Math.random() > 0.5;
+            BrickGridColor[i][j] = `#${hex6(Math.floor(Math.random() * 255), 2)}${hex6(Math.floor(Math.random() * 255), 2)}${hex6(Math.floor(Math.random() * 255), 2)}`;
+        }
     }
 }
 
@@ -94,9 +100,13 @@ function move(): void {
 }
 
 function drawBricks(): void {
-    for (let i = 0; i < BrickGrid.length; i++) {
-        if (BrickGrid[i]) {
-            colorRect((BrickWidth + 2) * i + 2, 2, BrickWidth, BrickHeight, 'blue');
+    const startX = Math.floor(CanvasGame.width - (BricksCountBerLine * (BrickWidth + 2))) / 2;
+    for (let i = 0; i < BrickLinesCount; i++) {
+        for (let j = 0; j < BricksCountBerLine; j++) {
+            if (1 || BrickGrid[i][j]) {
+                colorRect((BrickWidth + 2) * j + startX, (BrickHeight + 2) * i + 2, BrickWidth, BrickHeight,
+                    BrickGridColor[i][j]);
+            }
         }
     }
 }
@@ -122,7 +132,17 @@ function colorText(text: string, x: number, y: number, color: string): void {
     CanvasContext.fillStyle = color;
     CanvasContext.fillText(text, x, y);
 }
-
+/**
+ * Draw a rectangle on screen
+ * @description -Draw a rectangle on screen
+ * @function colorRect
+ * @param {number}topLeftX -X
+ * @param {number}topLeftY -Y
+ * @param {number}width -Width
+ * @param {number}height -Height
+ * @param {number}fillColor -color
+ * @returns {void} Nothing
+ */
 function colorRect(topLeftX: number, topLeftY: number, width: number, height: number, fillColor: string): void {
     CanvasContext.fillStyle = fillColor;
     CanvasContext.fillRect(topLeftX, topLeftY, width, height);
@@ -144,5 +164,41 @@ function colorCircle(centerX: number, centerY: number, radius: number, fillColor
     CanvasContext.fill();
     CanvasContext.closePath();
 }
+/**
+ * 
+ */
+class Brick {
+    private x: number;
+    private y: number;
+    private width: number;
+    private height: number;
+    private color: string;
+    private path: Path2D;
+    private visible: boolean;
 
+    public constructor(x: number, y: number, width: number, height: number, color: string = "black") {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.visible = false;
+        this.path = new Path2D();
+        this.path.rect(this.x, this.y, this.width, this.height);
+
+    }
+
+    public contains(x: number, y: number, ctx: CanvasRenderingContext2D): boolean {
+        return ctx.isPointInPath(this.path as any, x, y as any);
+    }
+    
+    /**
+     * draw
+     */
+    public draw(ctx: CanvasRenderingContext2D): void {
+        if (this.visible) {
+            ctx.fill(this.path as any);
+        }
+    }
+}
 window.onload = main;
